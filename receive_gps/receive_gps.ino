3b8205cc3ada2df93b6a20d9d69ae736058a6f52
Jamie Wang <sallyj.5555@gmail.com>
2024-10-27 04:57:10 -0400

    GPS parsing integrated and bearing calculated

--- receive_gps/receive_gps.ino
+++ receive_gps/receive_gps.ino
@@ -21,8 +21,14 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 
 // GPS //
+/*
+//Hardware Serial
 #define GPSSerial Serial1
 Adafruit_GPS GPS(&GPSSerial);
+*/
+// I2C
+Adafruit_GPS GPS(&Wire);
+
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
 #define GPSECHO false
@@ -102,10 +108,10 @@ void setup() {
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 
-
   Serial.println("Adafruit GPS library basic parsing test!");
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
+  // GPS.begin(9600);
+  GPS.begin(0x10);  // The I2C address to use is 0x10
   // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   // uncomment this line to turn on only the "minimum recommended" data
@@ -123,8 +129,7 @@ void setup() {
   delay(1000);
 
   // Ask for firmware version
-  GPSSerial.println(PMTK_Q_RELEASE);
-
+  GPS.println(PMTK_Q_RELEASE);
 
   Serial.println("ELEGOO OLED Test!");
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -179,7 +184,7 @@ void loop() {
       Serial.print(c);
     }
   // if a sentence is received, we can check the checksum, parse it...
-  /*
+  
   if (GPS.newNMEAreceived()) {
     // a tricky thing here is if we print the NMEA sentence, or data
     // we end up not listening and catching other sentences!
@@ -191,39 +196,32 @@ void loop() {
       return; // we can fail to parse a sentence in which case we should just wait for another
     }
   }
-  */
+  
+  double my_lat;
+  double my_lon;
   if (GPS.fix) {
     // Serial.println("GPS satellite found!");
-    float my_lat;
-    if(GPS.lat == 'N') {
-      my_lat = GPS.latitude;
-    } else {
-      my_lat = GPS.latitude * -1;
-    }
-    float my_lon;
-    if(GPS.lat == 'E') {
-      my_lon = GPS.longitude;
-    } else {
-      my_lon = GPS.longitude * -1;
-    }
-    sprintf(buffer, "%.4f,%.4f", my_lat, my_lon);
-  } else {
+    my_lat = GPS.latitudeDegrees;
+    my_lon = GPS.longitudeDegrees;
+
+    sprintf(buffer, "%.5f,%.5f", my_lat, my_lon);
+  } /* else {
     // Serial.println("GPS satellite not found!");
     sprintf(buffer, "GPS satellite not found!");
-  }
+  } */
   // Serial.println(buffer);
 
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
   if (rf95.available()) {
     // Should be a message for us now
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
 
     if (rf95.recv(buf, &len)) {
       digitalWrite(LED_BUILTIN, HIGH);
       RH_RF95::printBuffer("Received: ", buf, len);
       Serial.print("Got: ");
       Serial.println((char*)buf);
-       Serial.print("RSSI: ");
+      Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
 
       // Send a reply
@@ -237,9 +235,31 @@ void loop() {
     }
   }
 
-  // TODO: Calculate angle between GPS coordinates
-  float received_lat;
-  float received_lon;
+  double arr[2];
+  parse_gps(buf, arr);
+  double received_lat = arr[0];
+  double received_lon = arr[1];
+
+  float bearing = atan2(cos(my_lat)*sin(received_lat)-sin(my_lat)*cos(received_lat)*cos(received_lon-my_lon), sin(received_lon-my_lon)*cos(received_lat));
+  Serial.println(bearing);
+}
+
+void parse_gps(uint8_t* buf, double* arr) {
+  String LoRa_message = String((char*)buf);
+  String latitude_str;
+  String longitude_str;
+  
+  latitude_str = LoRa_message.substring(0, LoRa_message.indexOf(","));
+  longitude_str = LoRa_message.substring(LoRa_message.indexOf(",")+1);
+  char lat_c[10];
+  char lon_c[10];
+  latitude_str.toCharArray(lat_c, 10);
+  longitude_str.toCharArray(lon_c, 10);
+  double lat = atof(lat_c);
+  double lon = atof(lon_c);
+
+  arr[0] = lat;
+  arr[1] = lon;
 
 }
 
